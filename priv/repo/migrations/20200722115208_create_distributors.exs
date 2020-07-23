8ce34e35302a5d5421ac16e8793f40c18a92ea3c
k-maekawa <qian41119122@gmail.com>
2020-07-23 14:53:48 +0900

    init

---
+++ priv/repo/migrations/20200722115208_create_distributors.exs
@@ -0,0 +1,14 @@
+# priv/repo/migrations/*_create_distributors.exs
+
+defmodule Friends.Repo.Migrations.CreateDistributors do
+  use Ecto.Migration
+
+  def change do
+    create table(:distributors) do
+      add :name, :string
+      add :movie_id, references(:movies)
+    end
+
+    create unique_index(:distributors, [:movie_id])
+  end
+end
---
+++ priv/repo/migrations/20200722115608_create_actors.exs
@@ -0,0 +1,11 @@
+# priv/migrations/*_create_actors.ex
+
+defmodule Friends.Repo.Migrations.Actors do
+  use Ecto.Migration
+
+  def change do
+    create table(:actors) do
+      add :name, :string
+    end
+  end
+end
